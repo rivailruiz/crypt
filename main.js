04d1ae37961d82e7a0a55e8130187a8d456f13c2
Alexander Ruiz <AlexanderRuiz@Alexanders-MacBook-Air.local>
2017-12-01 00:08:07 -0200

    uaehua

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
 $(document).ready(function(){
-    $.support.cors = true;
-    $.ajax({
-        url: "https://bittrex.com/api/v1.1/public/getmarkets",
-        type: 'GET',
-        dataType: 'json',
-        method: 'jsonp',
-        // beforeSend: function (xhr) {
-        //     xhr.setRequestHeader ("Authorization", "Basic " + btoa(""));
-        // },
-        success: function (data){
-          var contador = data.result.length
-            data = data.result;
-          for (var i = 0; i < contador; i++){
-              var item = data[i];
-                // console.log(item);
-                
-              $('.container').append('<div class="col-md-2"><h3>'+item.MarketCurrencyLong+'</h3><img src="'+item.LogoUrl+'" alt=""><p>Minimo trade: <span>'+item.MinTradeSize+'</span></p></div>')
-             
-          }
-        },
-        error: function(error){
-            console.log(error);
-        }
-    })
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://bittrex.com/api/v1.1/public/getmarkets');
+    xhr.setRequestHeader("Accept", "application/vnd.github.3.raw");
+    xhr.send();
+    
+    xhr.onload = function(e){
+       var data = JSON.parse(xhr.response);
+       console.log(data);
+
+       var contador = data.result.length
+       data = data.result;
+       for (var i = 0; i < contador; i++){
+           var item = data[i];
+           // console.log(item);
+           
+         $('.container').append('<div class="col-md-2"><h3>'+item.MarketCurrencyLong+'</h3><img src="'+item.LogoUrl+'" alt=""><p>Minimo trade: <span>'+item.MinTradeSize+'</span></p></div>')
+        
+     }
+       
+    }
+
 });
 
 //https://bittrex.com/api/v1.1/public/getmarkets
