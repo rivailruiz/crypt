5400df91b2e6bd9a996c469854e37bf439719cde
Alexander Ruiz <AlexanderRuiz@Alexanders-MacBook-Air.local>
2017-11-30 22:52:18 -0200

    bla

---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $.support.cors = true;
+    $.ajax({
+        url: "https://bittrex.com/api/v1.1/public/getmarkets",
+        type: 'GET',
+        dataType: 'json',
+        method: 'jsonp',
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        // beforeSend: function (xhr) {
+        //     xhr.setRequestHeader ("Authorization", "Basic " + btoa(""));
+        // },
+        success: function (data){
+          console.log(data);
+        },
+        error: function(error){
+            console.log(error);
+        }
+    })
+});
+
+//https://bittrex.com/api/v1.1/public/getmarkets
